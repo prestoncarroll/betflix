58ae1afab481e88a7946c8a3c94a43993096f50e
Preston Carroll <plutusinc1@yahoo.com>
2020-07-20 13:01:33 -0500

    requests set, row component built, axios installed

---
+++ src/Row.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+
+function Row({title}) {
+    const [movies, setMovies] = useState([]);
+    
+
+    return (
+        <div>
+            <h2>{title}</h2>
+
+            {/* containerr -> posters */}
+            
+        </div>
+    )
+}
+
+export default Row
