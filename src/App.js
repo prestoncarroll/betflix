b7e8798db17bb32ad9bee01426895ccfb877735e
Preston Carroll <plutusinc1@yahoo.com>
2020-07-20 16:29:47 -0500

    x and y axis set, scroll bar now intact

--- src/App.js
+++ src/App.js
@@ -11,6 +11,12 @@ function App() {
 
       <Row title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals} />
       <Row title="Trending now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fecthTopRated} />
+      <Row title="Action" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
 
     </div>
 
